00f527403df6b5b48dd588768604ca089382737e
Adrian Burgos <adrian.burgos.colas@gmail.com>
2021-12-14 15:05:56 -0500

    start

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+
+    $("#menu-btn").click(function(){
+        $("#side-nav").css("width","100%");
+    });
+
+    $(".close-side-nav").click(function(){
+        $("#side-nav").css("width","0");
+    });
+});
