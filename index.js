ca2586c2b810d6166317307be9821bc1a4d90c80
Adrian Burgos <adrian.burgos.colas@gmail.com>
2021-12-15 03:37:30 -0500

    mobile side nav, mobile header and carousel-no-btn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    
+    function nextSlide(){
+        $(".translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
+        $(".-translate-x-full").removeClass("-translate-x-full").addClass("translate-x-full");
+
+        $(".translate-x-0").removeClass("translate-x-0").addClass("-translate-x-full");
+        $(".next").removeClass("translate-x-full").removeClass("next").addClass("translate-x-0")
+
+        $(".-translate-x-full").removeClass("visible").addClass("invisible")
+        
+
+        // $(".visible").removeClass("translate-x-full").addClass("translate-x-0");
+        // $(".translate-x-full").removeClass("invisible").addClass("visible")
+        // $(".invisible").removeClass("invisible").addClass("visible");
+        // $(".-translate-x-full").addClass("invisible").removeClass("visible");
+
+    };
 
     $("#menu-btn").click(function(){
         $("#side-nav").css("width","100%");
@@ -9,4 +24,11 @@ $(document).ready(function(){
         // $(this).css("background","white")
         $("#side-nav").css("width","0");
     });
+    $("#next").click(nextSlide);
+    var timer = setInterval(function(){
+        nextSlide();
+    },3000);
+    $(window).on("beforeunload", function() { 
+        clearInterval(timer);
+    })
 });
