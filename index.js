68fc8b791075109d3e02003d7a4f8db34a8fb71b
Adrian Burgos <adrian.burgos.colas@gmail.com>
2021-12-17 13:05:34 -0500

    added some frecodecamp projects cards

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
+
+    let slide = true;
+
     function nextSlide(){
-        $(".translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
-        $(".-translate-x-full").removeClass("-translate-x-full").addClass("translate-x-full");
-
-        $(".translate-x-0").removeClass("translate-x-0").addClass("-translate-x-full");
-        $(".next").removeClass("translate-x-full").removeClass("next").addClass("translate-x-0")
-
-        $(".-translate-x-full").removeClass("visible").addClass("invisible")
-        
-
-        // $(".visible").removeClass("translate-x-full").addClass("translate-x-0");
-        // $(".translate-x-full").removeClass("invisible").addClass("visible")
-        // $(".invisible").removeClass("invisible").addClass("visible");
-        // $(".-translate-x-full").addClass("invisible").removeClass("visible");
-
+        if(slide){
+            $(".translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
+            $(".-translate-x-full").removeClass("-translate-x-full").addClass("translate-x-full");
+            $(".translate-x-0").removeClass("translate-x-0").addClass("-translate-x-full");
+            $(".next").removeClass("translate-x-full").removeClass("next").addClass("translate-x-0")
+            $(".-translate-x-full").removeClass("visible").addClass("invisible")
+            setTimeout(function(){slide = true},1600)
+        }
+    };
+    
+    function prevSlide(){
+        if(slide){
+            $(".translate-x-full").addClass("invisible").removeClass("next")
+            $(".-translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
+            $(".translate-x-full").removeClass("translate-x-full").addClass("-translate-x-full");
+            $(".translate-x-0").removeClass("translate-x-0").addClass("translate-x-full");
+            $(".next").removeClass("-translate-x-full").removeClass("next").addClass("translate-x-0")
+            $(".translate-x-full").removeClass("visible").addClass("invisible")
+            setTimeout(function(){slide = true},1400)
+        }
     };
 
     $("#menu-btn").click(function(){
@@ -24,9 +33,26 @@ $(document).ready(function(){
         // $(this).css("background","white")
         $("#side-nav").css("width","0");
     });
-    $("#next").click(nextSlide);
+    $(".no-slide").hover(
+    function(){
+        clearInterval(timer);
+    },function(){
+        timer = setInterval(function(){
+            nextSlide();
+            slide = false
+        },3000);
+    });
+    $("#next").click(function(){
+        nextSlide();
+       slide = false;
+    });
+    $("#previous").click(function(){
+        prevSlide();
+       slide = false;
+    });
     var timer = setInterval(function(){
         nextSlide();
+        slide = false
     },3000);
     $(window).on("beforeunload", function() { 
         clearInterval(timer);
