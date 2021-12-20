ae3b2ed44015ceaa4917317b0bb5c937b1a865c4
Adrian Burgos <adrian.burgos.colas@gmail.com>
2021-12-19 23:16:47 -0500

    finished mobile view

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
 
     let slide = true;
+    let timer;
 
     function nextSlide(){
         if(slide){
-            $(".translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
+            $(".translate-x-full").removeClass("invisible").addClass("visible").addClass("next");
             $(".-translate-x-full").removeClass("-translate-x-full").addClass("translate-x-full");
             $(".translate-x-0").removeClass("translate-x-0").addClass("-translate-x-full");
-            $(".next").removeClass("translate-x-full").removeClass("next").addClass("translate-x-0")
-            $(".-translate-x-full").removeClass("visible").addClass("invisible")
-            setTimeout(function(){slide = true},1600)
+            $(".next").removeClass("translate-x-full").removeClass("next").addClass("translate-x-0");
+            $(".-translate-x-full").removeClass("visible").addClass("invisible");
+            timer = setTimeout(function(){slide = true},1600);
         }
     };
     
     function prevSlide(){
         if(slide){
-            $(".translate-x-full").addClass("invisible").removeClass("next")
+            $(".translate-x-full").addClass("invisible").removeClass("next");
             $(".-translate-x-full").removeClass("invisible").addClass("visible").addClass("next")
             $(".translate-x-full").removeClass("translate-x-full").addClass("-translate-x-full");
             $(".translate-x-0").removeClass("translate-x-0").addClass("translate-x-full");
-            $(".next").removeClass("-translate-x-full").removeClass("next").addClass("translate-x-0")
-            $(".translate-x-full").removeClass("visible").addClass("invisible")
-            setTimeout(function(){slide = true},1400)
+            $(".next").removeClass("-translate-x-full").removeClass("next").addClass("translate-x-0");
+            $(".translate-x-full").removeClass("visible").addClass("invisible");
+            timer = setTimeout(function(){slide = true},1400);
         }
     };
 
@@ -30,16 +31,15 @@ $(document).ready(function(){
     });
 
     $(".close-side-nav").click(function(){
-        // $(this).css("background","white")
         $("#side-nav").css("width","0");
     });
     $(".no-slide").hover(
     function(){
-        clearInterval(timer);
+        clearInterval(interval);
     },function(){
-        timer = setInterval(function(){
+        interval = setInterval(function(){
             nextSlide();
-            slide = false
+            slide = false;
         },3000);
     });
     $("#next").click(function(){
@@ -50,11 +50,12 @@ $(document).ready(function(){
         prevSlide();
        slide = false;
     });
-    var timer = setInterval(function(){
+    var interval = setInterval(function(){
         nextSlide();
-        slide = false
+        slide = false;
     },3000);
     $(window).on("beforeunload", function() { 
-        clearInterval(timer);
+        clearInterval(interval);
+        clearTimeout(timer);
     })
 });
